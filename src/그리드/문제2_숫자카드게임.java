ea5aa38bd08267c31cf56523c9004e4e7794917e
Tojaeung <kimwoon11@naver.com>
2022-09-16 17:44:08 +0900

    그리드 문제 복습

--- src/그리드/문제2_숫자카드게임.java
+++ src/그리드/문제2_숫자카드게임.java
@@ -29,5 +29,30 @@ public class 문제2_숫자카드게임 {
 
         System.out.println(result);
 
+        /*
+         * 나는 배열에 최솟값을 저장했다가 정렬 후 최대값을 반환하는 방법으로 풀이
+         * 카드맵을 만들고 마지막에 정렬을 하므로 시간복잡도가 O((N * M) + logN)로 생각한다.
+         * 그러나, 답안풀이는 for루프를 돌면서 할당하는 방법으로 심플하고 아름답게 구현하였다.
+         * 시간복잡도가 O(N * M)이다.
+         * 답안풀이가 더 효율적이다.
+         */
+        // Scanner sc = new Scanner(System.in);
+        //
+        // int n = sc.nextInt();
+        // int m = sc.nextInt();
+        // int[] arr = new int[n];
+        // int result = 0;
+        //
+        // for (int i = 0; i < n; i++) {
+        // int minValue = 10001;
+        // for (int j = 0; j < m; j++) {
+        // int num = sc.nextInt();
+        // minValue = Math.min(num, minValue);
+        // arr[i] = minValue;
+        // }
+        // }
+        //
+        // Arrays.sort(arr);
+        // System.out.println(arr[arr.length - 1]);
     }
 }
--- src/그리드/문제4_1이될때까지.java
+++ src/그리드/문제4_1이될때까지.java
@@ -9,60 +9,51 @@ public class 문제4_1이될때까지 {
         int n = sc.nextInt();
         int k = sc.nextInt();
 
-        /*
-         * 나의 답안
-         * */
-
         int cnt = 0;
 
-        long startTime = System.nanoTime();
-
         while (true) {
-            if (n == 1) break;
+            // N이 K로 나누어 떨어지는 수가 될 때까지만 1씩 빼기
+            int target = (n / k) * k;
+            cnt += (n - target);
+            n = target;
 
-            if (n % k == 0) {
-                n /= k;
-                cnt++;
-            } else {
-                n--;
-                cnt++;
+            /*
+             * n == 1 일때 반복문을 탈출한다면, 간단하지만 치명적인 오류가 있다.
+             * 바로 0으로 무한루프를 돌게된다. 그 이유는 target이 0이 되기 떄문이다. (자료형이 int이다.)
+             * 그래서 for루프를 n < k까지 돌려주고 나머지는 for루프를 탈출해서 해결해준다.
+             */
+            if (n < k) {
+                break;
             }
-
+            // K로 나누기
+            cnt += 1;
+            n /= k;
         }
-        long endTime = System.nanoTime();
-
-        System.out.println(cnt);
-        System.out.println("수행시간: " + (endTime - startTime));
-
 
         /*
-         * 문제집 답안
-         * 문제집 답안은 나머지를 cnt에 미리 더해서 루프를 감소시켰다.
-         * 그래서 나의 답안보다 문제집답안이 빠르고 효율적이라고 할 수 있겠다. (별루 차이는 안나는뎅;;;)
-         * */
+         * 마지막으로 남은 수에 대하여 1씩 빼기
+         * 다 뺴버리면 0이 되기 떄문에...
+         */
+        cnt += (n - 1);
+        System.out.println(cnt);
 
-        // int result = 0;
-        //
-        // long startTime = System.nanoTime();
+        /*
+         * 나의 풀이로도 풀수있지만... -1를 해주는 과정을 일일이 for루프를 돌면서 처리하기 떄문에
+         * 시간 복잡도가 위의 풀이보다 복잡해진다....
+         * 
+         */
         // while (true) {
-        //     // N이 K로 나누어 떨어지는 수가 될 때까지만 1씩 빼기
-        //     int target = (n / k) * k;
-        //     result += (n - target);
-        //     n = target;
-        //     // N이 K보다 작을 때 (더 이상 나눌 수 없을 때) 반복문 탈출
-        //     if (n < k) break;
-        //     // K로 나누기
-        //     result += 1;
-        //     n /= k;
+        // if (n == 1)
+        // break;
+
+        // if (n % k == 0) {
+        // n /= k;
+        // cnt++;
+        // } else {
+        // n--;
+        // cnt++;
         // }
-        //
-        // long endTime = System.nanoTime();
-        //
-        // // 마지막으로 남은 수에 대하여 1씩 빼기
-        // result += (n - 1);
-        // System.out.println(result);
-        // System.out.println("수행시간: " + (endTime - startTime));
 
-
+        // }
     }
 }
