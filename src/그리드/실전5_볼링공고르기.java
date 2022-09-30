e2a63b22225212dc5ae552f282f15b7a171c5e30
Tojaeung <kimwoon11@naver.com>
2022-09-30 20:28:58 +0900

    고난이도 문제풀이1

---
+++ src/그리드/실전5_볼링공고르기.java
@@ -0,0 +1,26 @@
+package 그리드;
+
+import java.util.Scanner;
+
+public class 실전5_볼링공고르기 {
+    public static int n, m;
+    public static int[] arr = new int[11];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            arr[sc.nextInt()] += 1;
+        }
+
+        int result = 0;
+        for (int i = 1; i <= m; i++) {
+            n -= arr[i];
+            result += arr[i] * n;
+        }
+
+        System.out.println(result);
+    }
+}
