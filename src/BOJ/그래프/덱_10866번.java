2b4943171ae3dc577f8e4e113e787a2faf64ac0b
Tojaeung <kimwoon11@naver.com>
2022-10-19 23:52:54 +0900

    BOJ 문제풀이 17일차

---
+++ src/BOJ/그래프/덱_10866번.java
@@ -0,0 +1,58 @@
+package BOJ.그래프;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class 덱_10866번 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        Deque<Integer> dq = new LinkedList<>();
+
+        while (n-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            int num;
+
+            switch (command) {
+                case "push_front":
+                    num = Integer.parseInt(st.nextToken());
+                    dq.offerFirst(num);
+                    break;
+                case "push_back":
+                    num = Integer.parseInt(st.nextToken());
+                    dq.offerLast(num);
+                    break;
+
+                case "pop_front":
+                    sb.append(dq.isEmpty() ? "-1\n" : dq.pollFirst() + "\n");
+                    break;
+                case "pop_back":
+                    sb.append(dq.isEmpty() ? "-1\n" : dq.pollLast() + "\n");
+                    break;
+
+                case "front":
+                    sb.append(dq.isEmpty() ? "-1\n" : dq.peekFirst() + "\n");
+                    break;
+                case "back":
+                    sb.append(dq.isEmpty() ? "-1\n" : dq.peekLast() + "\n");
+                    break;
+
+                case "size":
+                    sb.append(dq.size() + "\n");
+                    break;
+                case "empty":
+                    sb.append(dq.isEmpty() ? "1\n" : "0\n");
+                    break;
+            }
+
+        }
+        System.out.println(sb);
+    }
+}
---
+++ src/BOJ/그래프/큐_10845번.java
@@ -0,0 +1,55 @@
+package BOJ.그래프;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class 큐_10845번 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        // 큐일떄는 맨뒤의 값을 참조하는 메서드가 없기 떄문에 LinkedList를 사용하였다.
+        LinkedList<Integer> q = new LinkedList<>();
+
+        while (n-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String operation = st.nextToken();
+            int num;
+
+            switch (operation) {
+                case "push":
+                    num = Integer.parseInt(st.nextToken());
+                    q.offer(num);
+                    break;
+
+                case "pop":
+                    sb.append(q.isEmpty() ? "-1\n" : q.poll() + "\n");
+                    break;
+
+                case "size":
+                    sb.append(q.size() + "\n");
+                    break;
+
+                case "empty":
+                    sb.append(q.isEmpty() ? "1\n" : "0\n");
+                    break;
+
+                case "front":
+                    sb.append(q.isEmpty() ? "-1\n" : q.peekFirst() + "\n");
+                    break;
+
+                case "back":
+                    // 큐는 맨뒤에 값을 참조하는 메서드가 존재하지 않는다.
+                    sb.append(q.isEmpty() ? "-1\n" : q.peekLast() + "\n");
+                    break;
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
