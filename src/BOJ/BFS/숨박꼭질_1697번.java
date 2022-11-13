ec37cd8240b6b5aa089e2e3186728cf41135a9f1
Tojaeung <kimwoon11@naver.com>
2022-11-14 02:51:03 +0900

    BOJ 문제풀이 26일차

---
+++ src/BOJ/BFS/숨박꼭질_1697번.java
@@ -0,0 +1,24 @@
+package BOJ.BFS;
+
+import java.util.Scanner;
+
+public class 숨박꼭질_1697번 {
+    public static int n;    // 수빈 위치
+    public static int k;    // 동생 위치
+    public static int[] dp;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        k = sc.nextInt();
+
+        if (n > k) {    // 수빈이 위치가 더 클때
+            System.out.println(n - k);  // -1밖에 없다.
+        } else {
+
+        }
+
+
+    }
+}
